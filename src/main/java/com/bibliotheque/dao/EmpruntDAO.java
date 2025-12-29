2c1edfd45bf793a7e2d2432d63126a9c47f0848f
Maha0478 <154900735+Maha0478@users.noreply.github.com>
2025-12-29 14:55:42 +0100

    modified:   src/main/java/com/bibliotheque/dao/EmpruntDAO.java 	modified:   src/main/java/com/bibliotheque/dao/impl/EmpruntDAOImpl.java 	modified:   src/main/java/com/bibliotheque/service/EmpruntService.java

--- src/main/java/com/bibliotheque/dao/EmpruntDAO.java
+++ src/main/java/com/bibliotheque/dao/EmpruntDAO.java
@@ -5,29 +5,20 @@ import java.util.List;
 
 import com.bibliotheque.model.Emprunt;
 
-<<<<<<< HEAD
 /**
  * DAO interface for Loan entity
  */
-public interface EmpruntDAO extends GenericDAO<Emprunt, Integer> {
-    List<Emprunt> findByMemberId(Integer memberId);
-    List<Emprunt> findActiveByBookId(Integer bookId);
-}
-// this is a comment just for the test ,what do you
-=======
 public interface EmpruntDAO {
-
-    void save(Emprunt emprunt) throws SQLException;
-
+    void save(Emprunt e) throws SQLException;
     Emprunt findById(int id) throws SQLException;
-
     List<Emprunt> findAll() throws SQLException;
+    void update(Emprunt e) throws SQLException;
+    void delete(int id) throws SQLException;
 
-    void update(Emprunt emprunt) throws SQLException;
-
-    List<Emprunt> findByMembre(int membreId) throws SQLException;
-
+    List<Emprunt> findByMembre(int idMembre) throws SQLException;
     List<Emprunt> findEnCours() throws SQLException;
+    int countEmpruntsEnCours(int idMembre) throws SQLException;
 
-    int countEmpruntsEnCours(int membreId) throws SQLException;
+    List<Emprunt> findByMemberId(Integer memberId) throws SQLException;
+    List<Emprunt> findActiveByBookId(Integer bookId) throws SQLException;
 }

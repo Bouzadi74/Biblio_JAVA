9f7954ff3f921202ccfd706a90b6ebc380ca9339
Bouzadi74 <bouzadibadreddine1@gmail.com>
2025-12-29 14:18:32 +0100

    new file:   lib/mysql-connector-j-9.5.0.jar 	modified:   pom.xml 	new file:   src/test/java/com/bibliotheque/infra/DatabaseConnectionTest.class 	modified:   src/test/java/com/bibliotheque/infra/DatabaseConnectionTest.java

--- src/test/java/com/bibliotheque/infra/DatabaseConnectionTest.java
+++ src/test/java/com/bibliotheque/infra/DatabaseConnectionTest.java
@@ -1,15 +1,27 @@
 package com.bibliotheque.infra;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnectionTest {
 
-    @Test
-    void getInstance_returnsSingleton() {
-        DatabaseConnection instance = DatabaseConnection.getInstance();
-        assertNotNull(instance);
-    }
+    public static void main(String[] args) {
 
+    String url = "jdbc:mysql://127.0.0.1:3306/bib?useSSL=false&serverTimezone=UTC";
+    String user = "root";
+    String password = "12345678";
+
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to MySQL successfully!");
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
